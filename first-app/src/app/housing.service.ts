1cd1368d2cfeba1011ae1e1fc941d234dd2d8c9c
Marcelo Belo de Souza <dsp.desenvdesiteseprogramas@gmail.com>
2025-05-22 09:39:58 -0300

    FORMULARIO
    FORMULARIO ADICIONADO COM SUCESSO

--- first-app/src/app/housing.service.ts
+++ first-app/src/app/housing.service.ts
@@ -110,6 +110,7 @@ export class HousingService {
         laundry: true,
       },
     ];
+    
   constructor() { }
   getAllHousingLocations(): HousingLocation[] {
     return this.housingLocationList;
